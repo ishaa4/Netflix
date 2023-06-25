e41e8f862670c8e187afd14b332a5578f01da7d8
Isha <ishakatyayani4@gmail.com>
2023-06-26 01:12:03 +0530

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzrRLRJ2rcDxwKgP_kqNf638jDbgHAcp8",
+  authDomain: "netflix-ebe17.firebaseapp.com",
+  projectId: "netflix-ebe17",
+  storageBucket: "netflix-ebe17.appspot.com",
+  messagingSenderId: "1078186787047",
+  appId: "1:1078186787047:web:7b25d51cda9a4982626242",
+  measurementId: "G-0ETN777QLH"
+};
